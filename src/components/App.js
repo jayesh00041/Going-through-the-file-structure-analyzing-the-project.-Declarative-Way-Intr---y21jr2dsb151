ce4105f8e9ff2d6c887dc60ef5254dbe303db0bc
jayesh00041 <jayesh00041@gmail.com>
2022-06-28 23:46:27 +0530

    submit code

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,23 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+
+  const [isShown, setIsShown] = useState(false);
+
+  const handleClick = event => {
+    // 👇️ toggle shown state
+    setIsShown(current => !current);
+
+    // 👇️ or simply set it to true
+    // setIsShown(true);
+  };
   return (
     <div id="main">
-      // Do not alter the main div
+        <button id="click" onClick={handleClick}>Click me</button>
+        {/* 👇️ show elements on click */}
+      {isShown && (
+        <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      )}
     </div>
   );
 }
